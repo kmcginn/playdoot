a610375d7111abd6bf9fb43dfd0a9eb087918f06
Kevin McGinn <kmcginn14@gmail.com>
2022-05-23 21:06:08 -0700

    remove template items

--- source/main.lua
+++ source/main.lua
@@ -1,28 +1,21 @@
-import "dvd" -- DEMO
-local dvd = dvd(1, -1) -- DEMO
-
 local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
 
 local function loadGame()
-	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
+    playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
+    math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
 end
 
 local function updateGame()
-	dvd:update() -- DEMO
 end
 
 local function drawGame()
-	gfx.clear() -- Clears the screen
-	dvd:draw() -- DEMO
+    gfx.clear() -- Clears the screen
 end
 
 loadGame()
 
 function playdate.update()
-	updateGame()
-	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
+    updateGame()
+    drawGame()
+    playdate.drawFPS(0,0) -- FPS widget
 end
