9494b793ae70e3c9a9aae1c01cca5abbf1069517
Kevin McGinn <kmcginn14@gmail.com>
2022-06-03 20:10:26 -0700

    display max level of mic input

--- source/main.lua
+++ source/main.lua
@@ -1,15 +1,30 @@
 local gfx <const> = playdate.graphics
+local snd <const> = playdate.sound
+
+local maxLevel = 0.0
 
 local function loadGame()
     playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
     math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
+    snd.micinput.startListening()
 end
 
+local levelNum
+
 local function updateGame()
+    
+    levelNum = snd.micinput.getLevel()
+    -- snd.micinput.stopListening()
+    -- print(levelNum)
+    if levelNum > maxLevel then
+        maxLevel = levelNum
+    end
 end
 
 local function drawGame()
     gfx.clear() -- Clears the screen
+    local drawValue = maxLevel * 100
+    gfx.drawText(drawValue, 10, 10)
 end
 
 loadGame()
